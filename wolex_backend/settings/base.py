8bfe4873f261ad457d3d0124fcd9ddb2a2c41b46
Adedayo Wonderment <wondermentadedayo000@gmail.com>
2026-04-09 11:15:47 +0100

    fix: clean requirements.txt

--- wolex_backend/settings/base.py
+++ wolex_backend/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 
 from django import http
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
+from decouple import config
 
 from decouple import config
 
--- wolex_backend/settings/dev.py
+++ wolex_backend/settings/dev.py
@@ -4,3 +4,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+
+from django.core.management.utils import get_random_secret_key
+
+print(get_random_secret_key())
